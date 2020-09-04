dc4cf586d27c93dd9dd085e295b704609a41217c
Manuel_Ruiz <manuel_ruiz@ciencias.unam.mx>
2020-09-03 21:48:30 -0500

    sub

--- php/index2.php
+++ php/index2.php
@@ -71,13 +71,13 @@
                     </div>
                     <div class="tabs-content">
                         <div id="login-tab-content" class="active">
-                            <form class="login-form" action="../php/index1.php" method="POST" enctype="application/x-www-form-urlencoded">
-                                <input type="text" name="nombre_txt" class="input" id="user_login" autocomplete="off" placeholder="Correo electronico">
-                                <input type="password" name="password_txt" class="input" id="user_pass" autocomplete="off" placeholder="contraseña">
-                                <input type="checkbox" class="checkbox" checked id="remember_me">
-                                <label for="remember_me">Recordar contraseña</label>
-                                <input type="submit" name="enviar_btn" value="Iniciar sesión" class="button">
-                            </form>
+                                <form name="envia_form" action="index1.php" class="login-form" method="POST" enctype="application/x-www-form-urlencoded">
+                                    <input type="text" name="nombre_txt" class="input" id="user_login" autocomplete="off" placeholder="Correo electronico">
+                                    <input type="password" name="password_txt" class="input" id="user_pass" autocomplete="off" placeholder="contraseña">
+                                    <input type="checkbox" class="checkbox" checked id="remember_me">
+                                    <label for="remember_me">Recordar contraseña</label>
+                                    <input type="submit" name="enviar_btn" value="Iniciar sesión" class="button">
+                                </form>
                             <div class="help-action">
                                 <p><i class="fa fa-arrow-left" aria-hidden="true"><a href="#" class="forgot">¿Olvidaste tu contraseña?</a></i></p>
                             </div>
@@ -99,9 +99,7 @@
             </div>
         </div>
     </div>
-    <?php
-    include("conexion.php")
-    ?>  
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="../Jquery/script2.js"></script>
 </body>
